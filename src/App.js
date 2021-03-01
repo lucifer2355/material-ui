3ec101aa353553cbb2937c05c5f21286109c0961
“lucifer2355” <dgajjar999@gmail.com>
2021-03-01 14:33:11 +0530

    convert simplae function component to arraw function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className='app'>
       <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
