a0620abcf45e04d4d8e2ec057203c8a180861fda
“lucifer2355” <dgajjar999@gmail.com>
2021-03-01 14:51:21 +0530

    import all usefull component from material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container
+} from '@material-ui/core';
 
 const App = () => {
   return (
     <div className='app'>
-      <h1>Hello World</h1>
+      <Typography variant='h1'>Hello World</Typography>
     </div>
   );
 };
